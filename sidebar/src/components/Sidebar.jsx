082bef78ed9e5bdf9cf4969ef3ebe63ffd4d92eb
manishchouhan2396 <manishchouhan2396@gmail.com>
2022-06-02 08:47:39 +0530

    sidebar

---
+++ sidebar/src/components/Sidebar.jsx
@@ -0,0 +1,69 @@
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  // DrawerOverlay,
+  useDisclosure,
+  Flex,
+  CloseButton,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import {
+  FiHome,
+  FiTrendingUp,
+  FiCompass,
+  FiStar,
+  FiSettings,
+} from "react-icons/fi";
+import NavItem from "./NavItem";
+
+const Sidebar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [placement] = React.useState("left");
+
+  const LinkItems = [
+    { name: "Home", icon: FiHome },
+    { name: "Trending", icon: FiTrendingUp },
+    { name: "Explore", icon: FiCompass },
+    { name: "Favourites", icon: FiStar },
+    { name: "Settings", icon: FiSettings },
+  ];
+  return (
+    <Box>
+      <Button colorScheme="blue" onClick={onOpen} ml="10" mt="5">
+        Open
+      </Button>
+      <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
+        {/* <DrawerOverlay /> */}
+        <DrawerContent>
+          <DrawerBody>
+            <Flex
+              h="20"
+              alignItems="center"
+              mx="8"
+              justifyContent="space-between"
+            >
+              <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
+                Logo
+              </Text>
+              <CloseButton
+                display={{ base: "flex", md: "none" }}
+                onClick={onClose}
+              />
+            </Flex>
+            {LinkItems.map((link) => (
+              <NavItem key={link.name} icon={link.icon} name={link.name}>
+                {link.name}
+              </NavItem>
+            ))}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default Sidebar;
